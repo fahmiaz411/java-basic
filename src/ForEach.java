b7f40a503b2296c8b0802879d8bfa9d1632fcc5b
Fahmi <fahmiaziz884@gmail.com>
2023-08-04 21:47:24 +0700

    for each loop

---
+++ src/ForEach.java
@@ -0,0 +1,13 @@
+package src;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = {
+            1, 2, 3, 4
+        };
+
+        for (int n: arr) {
+            System.out.println(n);
+        }
+    }
+}
